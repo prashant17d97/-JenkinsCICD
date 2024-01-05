eae63d4c25d70a6bff55e5cd8ac7f42f4333cdf6
prasin <prashant.singh@openbet.com>
2024-01-05 17:11:56 +0530

    app centre

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -108,5 +108,7 @@ dependencies {
     androidTestImplementation(libs.ui.test.junit4)
     debugImplementation(libs.ui.tooling)
     debugImplementation(libs.ui.test.manifest)
+    implementation(libs.appcenter.analytics)
+    implementation(libs.appcenter.crashes)
 
 }
